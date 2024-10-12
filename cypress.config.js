00bc92d9c6e6e5a1b036fc5d0c1e76f4bdd7b585
Ahmed Mostafa <ahmed.moustafaa99@gmail.com>
2024-10-12 15:17:24 +0300

    Added report and videos

--- cypress.config.js
+++ cypress.config.js
@@ -6,8 +6,12 @@ module.exports = defineConfig({
     watchForFileChanges: false, // Disable watching for file changes
     chromeWebSecurity: false, // Disable Chrome web security
     blockHosts: ['https://events.backtrace.io'], // Block specified hosts
+    reporter: 'cypress-mochawesome-reporter',
+    video: true,
+    retries: 1,
     setupNodeEvents(on, config) {
       // Implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
